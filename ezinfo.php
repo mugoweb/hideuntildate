6015780e57feb69b72ca0b544a7db56a016267c8
Peter Keung <peter@mugo.ca>
2013-02-21 10:09:07 -0800

    Update copyright info

--- ezinfo.php
+++ ezinfo.php
@@ -3,7 +3,7 @@ class hideuntildateInfo
 {
     function info()
     {
-        return array( 'Name' => "Hide Until Date",
+        return array( 'Name' => "<a href='http://www.mugo.ca'>Mugo Hide Until Date</a>",
                       'Version' => "1.0",
                       'Copyright' => "Copyright (C) <a href='http://www.mugo.ca'>Mugo Web</a>",
                       'License' => "GNU General Public License v2.0"
